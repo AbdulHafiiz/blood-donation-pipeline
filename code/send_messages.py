a7f6988ceac19e4fa1b54ad297f91be2878e2f34
abdulinvoke <arhafiiz@gmail.com>
2024-01-22 21:31:00 +0800

    replaces send_messages function with send_documents, removed hardcoded values

--- code/send_messages.py
+++ code/send_messages.py
@@ -1,9 +1,5 @@
 import os
 import telebot
-import requests
-from time import sleep
-from datetime import datetime
-from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,51 +7,20 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 bot = telebot.TeleBot(BOT_TOKEN)
 
-def send_document(message:None, file:None, filename:None):
-    message_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?"
-    bot.send_message(
-        chat_id=CHAT_ID,
-        text=f"Malaysia's blood donation statistics for {current_date.date().strftime('%d-%m-%Y')}"
-    )
-
-    bot.send_document(
-        chat_id=CHAT_ID,
-        document=file,
-        caption=filename.replace(".png", "")
-    )
-    return
-
-
-def send_messages():
-    filenames = [
-        "Total Annual Blood Donations.png",
-        "Average Daily Blood Donations.png",
-        "Donation Retention Percentage.png",
-        "State Daily Donation Breakdown (Johor, Kedah, Kelantan, Melaka).png",
-        "State Daily Donation Breakdown (Negeri Sembilan, Pahang, Perak, Pulau Pinang).png",
-        "State Daily Donation Breakdown (Sabah, Sarawak, Selangor, Terengganu).png",
-        "State Daily Donation Breakdown (W.P. Kuala Lumpur).png",
-        "State Donation Breakdown (Johor, Kedah, Kelantan, Melaka).png",
-        "State Donation Breakdown (Negeri Sembilan, Pahang, Perak, Pulau Pinang).png",
-        "State Donation Breakdown (Sabah, Sarawak, Selangor, Terengganu).png",
-        "State Donation Breakdown (W.P. Kuala Lumpur).png",
-    ]
-    message_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?"
-    current_date = datetime.now(ZoneInfo("Asia/Kuala_Lumpur"))
-    bot.send_message(
-        chat_id=CHAT_ID,
-        text=f"Malaysia's blood donation statistics for {current_date.date().strftime('%d-%m-%Y')}"
-    )
-
-    for name in filenames:
-        bot.send_document(
+def send_document(message=None, file=None, filename=None):
+    if message:
+        bot.send_message(
             chat_id=CHAT_ID,
-            document=open(f"{PLOT_PATH}/{name}", "rb"),
-            caption=name.replace(".png", "")
+            text=message
         )
-        sleep(1)
+    if file:
+        bot.send_photo(
+            chat_id=CHAT_ID,
+            photo=file,
+            caption=filename.replace(".png", "") if filename else None
+        )
     return
 
 
 if __name__ == "__main__":
-    send_messages()
+    pass
